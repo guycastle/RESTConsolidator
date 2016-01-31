ac86dd771974b5154dd67c13e250cce75e881890
guillaume.vandecasteele <guillaume.vandecasteele@gmail.com>
2016-01-31 16:08:47 +0100

    Added annotation

--- src/main/java/be/vdab/valueobjects/SystemStatus.java
+++ src/main/java/be/vdab/valueobjects/SystemStatus.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Created on 30/01/2016 at 13:41.
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class SystemStatus {
