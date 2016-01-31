fac67a9d8a879043134445f747a839cda5b57b95
guillaume.vandecasteele <guillaume.vandecasteele@gmail.com>
2016-01-31 16:35:26 +0100

    Added HATEOAS support, revamped the class to make use of new Clients; an instance is created for every available URL from properties file. Reason the controller wasn't working before was because I autocompleted the RequestMapping path variable to params by mistake and didn't notice. Fixed now and everything works more or less.

--- src/main/java/be/vdab/restservices/SystemStatusController.java
+++ src/main/java/be/vdab/restservices/SystemStatusController.java
@@ -1,35 +1,60 @@
 package be.vdab.restservices;
 
-import be.vdab.restclients.APIEngineClient;
-import be.vdab.restclients.DigitalVaultClient;
+import be.vdab.exceptions.NoRESTClientPropertiesFoundException;
+import be.vdab.exceptions.NoSystemStatusFoundException;
+import be.vdab.restclients.CreateRESTClientBeans;
 import be.vdab.restclients.SystemStatusClient;
+import be.vdab.valueobjects.SystemStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created on 30/01/2016 at 14:24.
  */
 @RestController
-@RequestMapping(params = "/system")
-public class SystemStatusController {
+@RequestMapping(path = "/system")
+@ExposesResourceFor(SystemStatus.class)
+class SystemStatusController {
     private final List<SystemStatusClient> clients;
+    private final EntityLinks entityLinks;
 
     @Autowired
-    public SystemStatusController(APIEngineClient apiEngineClient, DigitalVaultClient digitalVaultClient) {
-        clients = new ArrayList<>();
-        clients.add(apiEngineClient);
-        clients.add(digitalVaultClient);
+    public SystemStatusController(@Qualifier("URLProperties") Properties URLs, @SuppressWarnings("SpringJavaAutowiringInspection") EntityLinks entityLinks) {
+        this.entityLinks = entityLinks;
+        this.clients = new ArrayList<>();
+        ApplicationContext context = new AnnotationConfigApplicationContext(CreateRESTClientBeans.class);
+        URLs.values().forEach(url -> clients.add(context.getBean(SystemStatusClient.class).setURL((String) url)));
+
     }
 
     @RequestMapping(method = RequestMethod.GET)
     SystemStatusesResource getStatuses() {
-        SystemStatusesResource systemStatusesResource = new SystemStatusesResource();
-        clients.forEach(client -> systemStatusesResource.addSystemStatus(client.getSystemStatus()));
-        return systemStatusesResource;
+        return new SystemStatusesResource(getAll(), entityLinks);
+    }
+
+    private Iterable<SystemStatus> getAll() {
+        List<SystemStatus> returnValue = new ArrayList<>();
+        clients.forEach(client -> returnValue.add(client.getSystemStatus()));
+        return returnValue;
+    }
+
+    @ExceptionHandler(NoSystemStatusFoundException.class)
+    @ResponseStatus(HttpStatus.FAILED_DEPENDENCY)
+    void getFailed() {
+    }
+
+    @ExceptionHandler(NoRESTClientPropertiesFoundException.class)
+    @ResponseStatus(HttpStatus.FAILED_DEPENDENCY)
+    void couldNotFindProperties() {
     }
 }
